54e51b3083c58e0c0f471a5a13e65f18819d1463
Frederik P. B. H <frepbh@gmail.com>
2021-05-24 02:27:22 +0200

    small change

--- src/error/generic_error.rs
+++ src/error/generic_error.rs
@@ -1,38 +1,39 @@
 use std::convert::From;
 use std::error::Error;
 use std::fmt::{self, Display, Formatter};
+use std::option::NoneError;
 
 #[derive(Debug)]
 pub struct GenericError(pub String);
 
 impl Display for GenericError {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        f.write_str(&self.0)
-    }
+	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+		f.write_str(&self.0)
+	}
 }
 
-impl From<std::option::NoneError> for GenericError {
-    fn from(_err: std::option::NoneError) -> Self {
-        GenericError(String::from("Option contained no values."))
-    }
+impl From<NoneError> for GenericError {
+	fn from(_err: NoneError) -> Self {
+		GenericError(String::from("Option contained no values."))
+	}
 }
 
 impl Error for GenericError {}
 
 impl From<&str> for GenericError {
-    fn from(val: &str) -> Self {
-        GenericError(val.to_owned())
-    }
+	fn from(val: &str) -> Self {
+		GenericError(val.to_owned())
+	}
 }
 
 impl From<String> for GenericError {
-    fn from(val: String) -> Self {
-        GenericError(val)
-    }
+	fn from(val: String) -> Self {
+		GenericError(val)
+	}
 }
 
 impl From<std::io::Error> for GenericError {
-    fn from(err: std::io::Error) -> Self {
-        GenericError(err.to_string())
-    }
+	fn from(err: std::io::Error) -> Self {
+		GenericError(err.to_string())
+	}
 }
