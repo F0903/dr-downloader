e1c0a743cb8f3c16fe2e8624d4974f98a4389089
Frederik P <frepbh@gmail.com>
2022-12-11 17:02:19 +0100

    made the cacher module public

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 #[macro_use]
 extern crate lazy_static;
 
+pub mod cacher;
 pub mod converter;
 pub mod downloader;
 pub mod error;
 pub mod requester;
 pub mod saver;
 
-mod cacher;
 mod event;
 mod models;
 mod util;
